0d273715a616e974524576bffa8a56b5519fb3ad
M7l35 <153141317+M7l35@users.noreply.github.com>
2023-12-07 20:16:49 +1100

    Create AsciiAnimationSystem.py
    A basic one line refreshing animation system for ascii

---
+++ AsciiAnimationSystem.py
@@ -0,0 +1,37 @@
+import time,os
+os.system('clear')
+
+tme = 0.1 # time between frames
+
+Loop_Amount=5 # variable for amount of full loops
+
+x=0 # variable used to start and stop loops
+while True:
+    print('o-----',end='\r')
+    time.sleep(tme)
+    print('-o----',end='\r')
+    time.sleep(tme)
+    print('--o---',end='\r')
+    time.sleep(tme)
+    print('---o--',end='\r')
+    time.sleep(tme)
+    print('----o-',end='\r')
+    time.sleep(tme)
+    print('-----o',end='\r')
+    time.sleep(tme)
+    print('-----o',end='\r')
+    time.sleep(tme)
+    print('----o-',end='\r')
+    time.sleep(tme)
+    print('---o--',end='\r')
+    time.sleep(tme)
+    print('--o---',end='\r')
+    time.sleep(tme)
+    print('-o----',end='\r')
+    time.sleep(tme)
+    print('o-----',end='\r')
+    time.sleep(tme)
+    x += 1
+    if x>=Loop_Amount:
+        break
+x=0
